38aee7eac2f4a170253d152805601908c5fd86f4
William Pereira <pereirawe@gmail.com>
2020-06-06 23:31:42 -0300

    Nuevos productos premium

--- layout/nav-products.php
+++ layout/nav-products.php
@@ -9,5 +9,13 @@
                     <a id="nav_product_8" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_8.php">LED Luminaria Exterior IP</a>
                     <a id="nav_product_9" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_9.php">IV High Bay Interior</a>
                     <a id="nav_product_10" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_10.php">iK Series Luz de Calle</a>
-                    <a id="nav_product_10" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_11.php">HB Iluminación linear</a>
+                    <a id="nav_product_11" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_11.php">HB Iluminación linear</a>
+                    <a id="nav_product_12" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_12.php">AoFS Alta Potencia</a>
+                    <a id="nav_product_13" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_13.php">BH AIO - Lampara Solar</a>
+                    <a id="nav_product_14" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_14.php">Panel PL40 Ultra Slim</a>
+                    <a id="nav_product_15" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_15.php">HY Iluminación exterior alta potencia</a>
+                    <a id="nav_product_16" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_16.php">IM Luz de calle</a>
+                    <a id="nav_product_17" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_17.php">Lampara Solar 360°</a>
+                    <a id="nav_product_18" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_18.php">SH21 Iluminación exterior</a>
+                    <a id="nav_product_19" class="list-group-item list-group-item-action" href="<?= $httpHost ?>productos/premium/iluminacion_19.php">TRD-P TRI-PROOF HERMÉTICA</a>
                 </ul>
--- productos/premium/iluminacion_14.php
+++ productos/premium/iluminacion_14.php
@@ -2,7 +2,7 @@
     include ('../../layout/header.php');
     include ('../../layout/nav.php');
 
-    $urlPdf = '7 FICHA TECNICA Lampara Solar IA LED BLITZ.pdf';
+    $urlPdf = '14 FICHA TECNICA Panel LEDBLITZ PL40 Ultra Slim.pdf';
 
 ?>
     <div class="container-fluid">
@@ -12,21 +12,19 @@
                 <?php include ('../../layout/nav-products.php'); ?>
             </div>
             <div class="col-md-9 pt-3 pb-5">
-                <h2>Iluminación solar exterior</h2>
+                <h2>Panel PL40 Ultra Slim</h2>
                 <div class="row">
                     <div class="col-md-12">
                         <embed id="pdf_embed" src="<?= $httpHost ?>pdf/<?= urldecode($urlPdf)?>" width="100%" height="550"  type="application/pdf">
                     </div>
                     <a href="<?= $httpHost ?>pdf/<?= urldecode($urlPdf)?>" type="button" class="btn btn-primary ml-auto mr-4" target="_new">Descargar Ficha Tecnica.PDF</a>
                 </div>
-
             </div>
-            
         </div>
     </div>
 <script>
     $('#nav_productos').addClass('active')
-    $('#nav_product_7').addClass('active')
+    $('#nav_product_14').addClass('active')
 </script>
 <?php
     // FOOTER
